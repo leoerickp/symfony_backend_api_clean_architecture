0705e5cf6eb4649de34856351ece846ab044e5be
Leo Erick Pereyra Rodriguez <leoerickp@gmail.com>
2026-04-14 11:12:25 -0400

    adding upload product image

--- src/Infrastructure/Controller/FilesController.php
+++ src/Infrastructure/Controller/FilesController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Infrastructure\Controller;
 
 use App\Application\UseCase\File\GetStaticProductImage;
+use App\Application\Exception\BadRequestException;
 use App\Infrastructure\Http\Response\SuccessResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
@@ -12,6 +13,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 final class FilesController extends AbstractController
 {
@@ -21,11 +23,42 @@ final class FilesController extends AbstractController
     ) {
     }
 
-    #[Route('/api/files/upload', name: 'app_files_upload', methods: ['POST'])]
-    public function upload(): JsonResponse
+    #[Route('/api/files/product', name: 'app_files_product', methods: ['POST'])]
+    public function uploadProductImage(Request $request): JsonResponse
     {
+        $file = $request->files->get('file');
+        if (!$file) {
+            throw new BadRequestException('No file provided');
+        }
 
-        return new SuccessResponse($this->serializer);
+        // Validate MIME type (equivalent to fileFilter)
+        if (!str_starts_with($file->getMimeType(), 'image/')) {
+            throw new BadRequestException('File must be an image');
+        }
+
+        // Validate file size (equivalent to fileSize)
+        if ($file->getSize() > 1024 * 1024 * 5) {
+            throw new BadRequestException('File must be less than 5MB');
+        }
+
+        // Generate unique name (equivalent to fileNamer)
+        $fileName = uniqid() . '.' . $file->guessExtension();
+
+        try {
+            $file->move(
+                $this->getParameter('kernel.project_dir') . '/src/static/products/',
+                $fileName
+            );
+        } catch (\Throwable $th) {
+            throw new \Exception('Error uploading file');
+        }
+
+        // Public URL
+        $secureUrl = $request->getSchemeAndHttpHost() . '/api/files/product/' . $fileName;
+
+        return new SuccessResponse($this->serializer, [
+            'url' => $secureUrl
+        ]);
     }
 
     #[Route('/api/files/product/{imageName}', name: 'app_files_product_image', methods: ['GET'])]
